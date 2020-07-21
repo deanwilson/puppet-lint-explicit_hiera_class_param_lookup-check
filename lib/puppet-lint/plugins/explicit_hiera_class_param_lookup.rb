45405957484b39976114053e218e5621f51665d7
Dean Wilson <dean.wilson@gmail.com>
2020-07-21 08:07:35 +0100

    Address the rubocop violations

--- lib/puppet-lint/plugins/explicit_hiera_class_param_lookup.rb
+++ lib/puppet-lint/plugins/explicit_hiera_class_param_lookup.rb
@@ -1,23 +1,22 @@
 PuppetLint.new_check(:explicit_hiera_class_param_lookup) do
   def check
     class_indexes.each do |class_idx|
-
       next unless class_idx[:param_tokens].is_a? Array
 
-      class_idx[:param_tokens].select { |t|
+      tokens = class_idx[:param_tokens].select do |t|
         (t.type == :NAME or t.type == :FUNCTION_NAME) and t.value == 'hiera'
-      }.each do |token|
+      end
 
+      tokens.each do |token|
         next unless token.next_code_token.type == :LPAREN
 
         hiera_key = token.next_code_token.next_code_token
 
         notify :error, {
-          :message => "explicit hiera() lookup of #{hiera_key.value}",
-          :line    => hiera_key.line,
-          :column  => hiera_key.column,
+          message: "explicit hiera() lookup of #{hiera_key.value}",
+          line:    hiera_key.line,
+          column:  hiera_key.column,
         }
-
       end
     end
   end
